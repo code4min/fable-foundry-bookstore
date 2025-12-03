94a81a237c75d5fb0de443a0fcfaf34abdd82c8c
Sneha <sneha4wq@gmail.com>
2025-12-03 20:57:23 +0530

    Update ProtectedRoute.js

--- bookstore-frontend/src/components/ProtectedRoute.js
+++ bookstore-frontend/src/components/ProtectedRoute.js
@@ -2,22 +2,22 @@
 import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
-// This component will protect both normal and admin routes
+
 const ProtectedRoute = ({ requiredRole }) => {
   const token = localStorage.getItem("token");
   const normalizedRole = localStorage.getItem("role_normalized");
 
-  // If no token, user is not logged in
+
   if (!token) {
     return <Navigate to="/login" replace />;
   }
 
-  // If a specific role is required (like ADMIN), check it
+ 
   if (requiredRole && normalizedRole !== requiredRole.toUpperCase()) {
     return <Navigate to="/unauthorized" replace />;
   }
 
-  // Otherwise, allow access
+  
   return <Outlet />;
 };
 
